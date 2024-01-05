724bf9fcda5d61d32b2400ec2252ccc531953dc8
Andreea Nitu <andreea.nitu@wirtek.com>
2024-01-05 14:00:58 +0200

    feat(): Fix android splash screen and add type check for navigation.

---
+++ src/Pages/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Button, Text, View } from 'react-native';
+import { HomeScreenProps } from '../NavigationTypes';
+
+export default function HomeScreen({navigation} : HomeScreenProps) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home</Text>
+        <Button title='Navigate to categories' onPress={()=> navigation.navigate("Categories",{})}></Button>
+      </View>
+    );
+}
