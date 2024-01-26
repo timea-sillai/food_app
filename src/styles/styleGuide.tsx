a89e3b83d2e6f9d67998734aafae7d761a136084
AndreeaMihaelaNitu <153748233+AndreeaMihaelaNitu@users.noreply.github.com>
2024-01-26 17:49:38 +0200

    feat(): add search bar functionality (#6)

---
+++ src/styles/styleGuide.tsx
@@ -0,0 +1,8 @@
+export const primary = {
+  green: "#40AA54",
+  white: "#FFFFFF",
+  light_green: "#F1FFF4",
+  grey_light: "#FAFAFA",
+  greenDO: "#D0F2D6",
+  search_bar_border_color: "#D0F2D6",
+};
