c7515a78f462fa20344b59d0b86f284597642400
Andreea Nitu <andreea.nitu@wirtek.com>
2024-02-05 14:18:53 +0200

    feat(): add svg library and update png files

--- src/pages/CategoriesScreen.tsx
+++ src/pages/CategoriesScreen.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { Text, View } from "react-native";
+import Categories from "../components/categoriesScreen/Categories";
 
 export default function CategoriesScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Categories</Text>
+    <View>
+      <Categories></Categories>
     </View>
   );
 }
